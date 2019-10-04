b342e0ee36101768b49149a0760edb9ac6f573ea
Angelo <7332321+antjowie@users.noreply.github.com>
2019-10-05 00:30:06 +0200

    Created solution and added SFML

---
+++ projects/engine/include/Engine/Framework/Engine.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace sh
+{
+    class Engine
+    {
+    public:
+
+        void Run();
+
+    private:
+    };
+}
---
+++ projects/game/src/Main.cpp
@@ -0,0 +1,8 @@
+#include <Engine/Framework/Engine.h>
+
+int main()
+{
+    sh::Engine engine;
+    engine.Run();
+    return 0;
+}
