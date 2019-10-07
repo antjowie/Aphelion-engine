cacff2cdfa22598e63a9f275bd74dd7bb7cd67cd
Angelo <7332321+antjowie@users.noreply.github.com>
2019-10-07 22:01:57 +0200

    Added Doxyfile for documentation

--- Engine/Framework/Engine.h
+++ Engine/Framework/Engine.h
@@ -2,12 +2,21 @@
 
 namespace sh
 {
+    /** \ingroup group_framework
+     * The entrypoint of the engine.
+     *
+     * This class is the framework that the engine runs on. It is responsible
+     * for starting and shutting down subsystems.
+     */
     class Engine
     {
     public:
 
+        /**
+         * Start the loop of the engine.
+         *
+         * This function has to be called to start up the engine
+         */
         void Run();
-
-    private:
     };
 }
