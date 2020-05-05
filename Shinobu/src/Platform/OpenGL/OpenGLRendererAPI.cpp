1167c8a2a595a2360bcfd5e95e295ff06e2b2b09
Angelo <7332321+antjowie@users.noreply.github.com>
2020-05-05 17:21:27 +0200

    Enabled face culling for OpenGL

--- Shinobu/src/Platform/OpenGL/OpenGLRendererAPI.cpp
+++ Shinobu/src/Platform/OpenGL/OpenGLRendererAPI.cpp
@@ -37,6 +37,7 @@ namespace sh
 		glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DEBUG_SEVERITY_NOTIFICATION, 0, NULL, GL_FALSE);
 #endif
 
+		glEnable(GL_CULL_FACE);
 		glEnable(GL_BLEND);
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
