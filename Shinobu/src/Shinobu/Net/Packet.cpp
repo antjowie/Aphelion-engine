0afa513e6d2e23568a20c6554b4fda9d794b3a03
Angelo <7332321+antjowie@users.noreply.github.com>
2020-07-09 16:07:22 +0200

    Removed redundant code from packet header and source

--- Shinobu/src/Shinobu/Net/Packet.cpp
+++ Shinobu/src/Shinobu/Net/Packet.cpp
@@ -9,31 +9,3 @@ namespace sh
         return enet_packet_create(packet.buffer.data(), packet.size, ENET_PACKET_FLAG_UNSEQUENCED);
     }
 }
-
-#if 0
-std::unique_ptr<Packet> PacketFromBinary(unsigned char* binary, unsigned count)
-{
-    // TODO: Find a way to make a stream use an existing buffer
-    // instead of copying it
-    std::stringstream stream;
-    for (size_t i = 0; i < count; i++)
-    {
-        stream << binary[i];
-    }
-
-    return std::move(PacketFromBinary(stream));
-}
-
-std::unique_ptr<Packet> PacketFromBinary(std::stringstream& binary)
-{
-    std::unique_ptr<Packet> packet;
-
-    {
-        cereal::PortableBinaryInputArchive in(binary);
-        in (packet);
-    }
-
-    return std::move(packet);
-}
-
-#endif
--- Shinobu/src/Shinobu/Net/Packet.h
+++ Shinobu/src/Shinobu/Net/Packet.h
@@ -18,11 +18,11 @@ namespace sh
 
     //define how object should be serialized/deserialized
     template <typename S>
-    void serialize(S& s, ExampleData& o) 
+    void serialize(S& s, ExampleData& o)
     {
-        s.text1b(o.message,128);
+        s.text1b(o.message, 128);
     }
-    
+
     /**
      * A packet is a piece of data that is transmitted via the network
      * It is what the client and server send to each other
@@ -60,120 +60,4 @@ namespace sh
     }
 
     _ENetPacket* SHINOBU_API MakeENetPacket(const Packet& packet);
-    
-#if 0
-    int main() {
-        //set some random data
-        MyStruct data{ 8941, MyEnum::V2, 0.045 };
-        MyStruct res{};
-
-        //open file stream for writing and reading
-        auto fileName = "test_file.bin";
-        std::fstream s{ fileName, s.binary | s.trunc | s.out };
-        if (!s.is_open()) {
-            std::cout << "cannot open " << fileName << " for writing\n";
-            return 0;
-        }
-
-        //we cannot use quick serialization function, because streams cannot use writtenBytesCount method
-        bitsery::Serializer<bitsery::OutputBufferedStreamAdapter> ser{ s };
-        ser.object(data);
-        //flush to writer
-        ser.adapter().flush();
-        s.close();
-        //reopen for reading
-
-        s.open(fileName, s.binary | s.in);
-        if (!s.is_open()) {
-            std::cout << "cannot open " << fileName << " for reading\n";
-            return 0;
 }
-
-        //same as serialization, but returns deserialization state as a pair
-        //first = error code, second = is buffer was successfully read from begin to the end.
-        auto state = bitsery::quickDeserialization<bitsery::InputStreamAdapter>(s, res);
-
-        assert(state.first == bitsery::ReaderError::NoError && state.second);
-        assert(data.f == res.f && data.i == res.i && data.e == res.e);
-}
-
-
-    //include bitsery.h to get serialization and deserialization classes
-    #include <bitsery/bitsery.h>
-    //in ordered to serialize/deserialize data to buffer, include buffer adapter
-    #include <bitsery/adapter/buffer.h>
-    //bitsery itself doesn't is lightweight, and doesnt include any unnessessary files,
-    //traits helps library to know how to use types correctly,
-    //in this case we'll be using vector both, to serialize/deserialize data and to store use as a buffer.
-    #include <bitsery/traits/vector.h>
-
-    enum class MyEnum :uint16_t { V1, V2, V3 };
-    struct MyStruct {
-        uint32_t i;
-        MyEnum e;
-        std::vector<float> fs;
-    };
-
-    //define how object should be serialized/deserialized
-    template <typename S>
-    void serialize(S& s, MyStruct& o) {
-        s.value4b(o.i);//fundamental types (ints, floats, enums) of size 4b
-        s.value2b(o.e);
-        s.container4b(o.fs, 10);//resizable containers also requires maxSize, to make it safe from buffer-overflow attacks
-    }
-#endif
-}
-
-// Legacy code
-#if 0
-#include <cereal/archives/portable_binary.hpp>
-#include <cereal/types/polymorphic.hpp>
-#include <cereal/types/memory.hpp>
-#include <cereal/types/base_class.hpp>
-
-#include <sstream>
-#include <memory>
-
-enum class PacketType
-{
-    JoinResponse,
-    EntityCommand
-};
-
-struct Packet
-{
-public:
-    virtual PacketType GetType() const = 0;
-
-    unsigned packetID = 1;
-
-    template <typename Archive>
-    void serialize(Archive& ar)
-    {
-        ar(packetID);
-    }
-};
-
-// Must be a smart pointer
-template <typename T>
-std::stringstream PacketToBinary(const T& packet)
-{
-    static_assert(
-        std::is_base_of<std::unique_ptr<Packet>::element_type,
-        T::element_type>::value, "T has to be derived from Packet");
-    std::stringstream stream;
-    {
-        cereal::PortableBinaryOutputArchive out(stream);
-        
-        // NOTE: Cereal needs a pointer of base type
-        // since it is too much of a hassle imo to convert all pointers to that type
-        // I just convert it here. There may be some overhead by doing that
-        std::unique_ptr<Packet> data(new T::element_type(*packet));
-        out(data);
-    }
-
-    return stream;
-}
-std::unique_ptr<Packet> PacketFromBinary(std::stringstream& binary);
-std::unique_ptr<Packet> PacketFromBinary(unsigned char* binary, unsigned count);
-#endif
